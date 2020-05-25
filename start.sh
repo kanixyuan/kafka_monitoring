b254edcc4516839bb05533fafaa24148953d4a10
Wang <kanixwang@fetca712057.onmicrosoft.com>
2020-05-25 15:57:45 +0800

    Add command for Cadvisor

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker-compose build --no-cache grafana
-docker-compose --compatibility up --force-recreate -d
+docker-compose --compatibility up --force-recreate -d --remove-orphans
 sleep 5
 echo 'zoo1 : '
 echo mntr | nc localhost 2181
